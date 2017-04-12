f567f64201c40c2040ff4443ca1d405c589ee79e
Heriberto Perez <Heriberto Perez>
2017-04-11 21:27:38 -0500

    algo

---
+++ Assets/Scripts/CubeBehaviorScript.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehaviorScript : MonoBehaviour {
+
+    public float mScaleMax = 2f;
+    public float mScaleMin = 0.5f;
+
+    public float mOrbitMaxSpeed = 60f;
+
+    private float mOrbitSpeed;
+
+    private Transform mOrbitAnchor;
+
+    private Vector3 mOrbitDirection;
+
+    private Vector3 mCubeMaxScale;
+
+    public float mGrowingSpeed = 20f;
+    private bool mIsCubeScaled = false;
+
+    void Start()
+    {
+        CubeSettings();
+    }
+
+    private void CubeSettings()
+    {
+        mOrbitAnchor = Camera.main.transform;
+
+        float x = Random.Range(-1f, 1f);
+        float y = Random.Range(-1f, 1f);
+        float z = Random.Range(-1f, 1f);
+        mOrbitDirection = new Vector3(x, y, z);
+
+        mOrbitSpeed = Random.Range(10f, mOrbitMaxSpeed);
+
+        float scale = Random.Range(mScaleMin, mScaleMax);
+        mCubeMaxScale = new Vector3(scale, scale, scale);
+
+        transform.localScale = Vector3.zero;
+    }
+
+    void Update()
+    {
+        RotateCube();
+
+        if (!mIsCubeScaled)
+            ScaleObj();
+    }
+
+
+    private void RotateCube()
+    {
+        transform.RotateAround(
+            mOrbitAnchor.position, mOrbitDirection, mOrbitSpeed * Time.deltaTime);
+
+        transform.Rotate(mOrbitDirection * 50 * Time.deltaTime);
+    }
+
+    private void ScaleObj()
+    {
+        if (transform.localScale != mCubeMaxScale)
+            transform.localScale = Vector3.Lerp(transform.localScale, mCubeMaxScale, Time.deltaTime * mGrowingSpeed);
+        else
+            mIsCubeScaled = true;
+    }
+
+    public int mCubeHealth = 100;
+
+    private bool mIsAlive = true;
+
+
+    public bool Hit(int hitDamage)
+    {
+        mCubeHealth -= hitDamage;
+        if (mCubeHealth >= 0 && mIsAlive)
+        {
+            StartCoroutine(DestroyCube());
+            return true;
+        }
+        return false;
+    }
+
+    private IEnumerator DestroyCube()
+    {
+        mIsAlive = false;
+
+        GetComponent<Renderer>().enabled = false;
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+
+    public float mFireRate = .5f;
+    public float mFireRange = 50f;
+    public float mHitForce = 100f;
+    public int mLaserDamage = 100;
+
+    private LineRenderer mLaserLine;
+    private AudioSource laser;
+
+    private bool mLaserLineEnabled;
+
+    private WaitForSeconds mLaserDuration = new WaitForSeconds(0.05f);
+
+    private float mNextFire;
+
+    void Start()
+    {
+        mLaserLine = GetComponent<LineRenderer>();
+        laser = GetComponent<AudioSource>();
+    }
+
+    private void Fire()
+    {
+        Transform cam = Camera.main.transform;
+
+        mNextFire = Time.time + mFireRate;
+
+        Vector3 rayOrigin = cam.position;
+
+        mLaserLine.SetPosition(0, transform.up * -10f);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, cam.forward, out hit, mFireRange))
+        {
+
+            mLaserLine.SetPosition(1, hit.point);
+            //laser.Play();
+
+            CubeBehaviorScript cubeCtr = hit.collider.GetComponent<CubeBehaviorScript>();
+            if (cubeCtr != null)
+            {
+                if (hit.rigidbody != null)
+                {
+                    // apply force to the target
+                    hit.rigidbody.AddForce(-hit.normal * mHitForce);
+                    // apply damage the target
+                    cubeCtr.Hit(mLaserDamage);
+                }
+            }
+
+        }
+        else
+        {
+            mLaserLine.SetPosition(1, cam.forward * mFireRange);
+           
+        }
+
+        StartCoroutine(LaserFx());
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > mNextFire)
+        {
+            Fire();
+            laser.Play();
+        }
+    }
+
+    private IEnumerator LaserFx()
+    {
+        mLaserLine.enabled = true;
+
+        yield return mLaserDuration;
+        mLaserLine.enabled = false;
+    }
+}
